195d25da7b7c1711a267106bcb029df15496c428
daniel <danzdroidel@gmail.com>
2022-12-23 23:46:05 +0700

    init

---
+++ src/main/java/com/Nusantara/NusantaraApplication.java
@@ -0,0 +1,13 @@
+package com.Nusantara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NusantaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NusantaraApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/Nusantara/model/entity/Provinsi.java
@@ -0,0 +1,4 @@
+package com.Nusantara.model.entity;
+
+public class Provinsi {
+}
